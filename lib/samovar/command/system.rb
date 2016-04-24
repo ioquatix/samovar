43776abb49ed8534b2d8e05f65fd4e0a07d3195b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-04-24 15:58:52 +1200

    Provide easy to use system/system! commands that show what is being executed.

---
+++ lib/samovar/command/system.rb
@@ -0,0 +1,54 @@
+# Copyright, 2016, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'time'
+
+module Samovar
+	class Command
+		class SystemError < RuntimeError
+		end
+		
+		def system(*args, **options)
+			pid = Process.spawn(*args, **options)
+			
+			command_line = args.join(' ')
+			puts Rainbow(command_line).color(:blue)
+			
+			status = Process.waitpid2(pid).last
+			
+			return status.success?
+		end
+		
+		def system!(*args, **options)
+			pid = Process.spawn(*args, **options)
+			
+			command_line = args.join(' ')
+			puts Rainbow(command_line).color(:blue)
+			
+			status = Process.waitpid2(pid).last
+			
+			if status.success?
+				return true
+			else
+				raise SystemError.new("Command #{command_line} failed#{status.to_s}")
+			end
+		end
+	end
+end
